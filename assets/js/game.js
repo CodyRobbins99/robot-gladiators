a68d381249258a3dab8e15a99c474864b2b7384a
Cody robbins <cody@Codys-MacBook-Pro.local>
2020-06-19 18:56:31 -0400

    simplified shop function input values

--- assets/js/game.js
+++ assets/js/game.js
@@ -144,23 +144,22 @@ var endGame = function() {
 var shop = function() {
     // Ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+    
     // Use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             // Increase health and decrease money
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             // increase attack and decrease money
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // Do nothing, so function will end
